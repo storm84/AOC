c8b27333a1a04c5e1f9a009dd03d036a579b9a20
Marcus Storm <storm.marcus@hotmail.com>
2019-12-01 08:34:30 +0100

    init commit ACO19

---
+++ AOC19/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AOC19
+{
+ class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = 0;
+            if(args.Length > 0)
+                int.TryParse(args[0],out day);
+            
+            switch(day)
+            {
+                case 1: new Aoc01($"data/input{day}.txt").run(); break;
+                //case 2: new Aoc02().run(); break;
+                //case 3: new Aoc03().run(); break;
+                //case 4: new Aoc04().run(); break;
+                default: Console.WriteLine("Invalid parameter or day is not implemented"); break;
+            }
+        }
+    }
+}
---
+++ AOC19/appclass/Aoc01.cs
@@ -0,0 +1,19 @@
+namespace AOC19
+{
+    class Aoc01 : AocBase
+    {
+        public Aoc01(string filepath) : base(@"data/input1.txt")
+        {
+        }
+
+        public override string executeA(string[] inputs)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string executeB(string[] inputs)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
