38a32d30ffd558b3c2354584ec542dd40c2ee736
xxstormm <ext.marcus.storm@tieto.com>
2019-12-02 09:30:14 +0100

    Day2 implemented

---
+++ AOC19/appclass/Aoc02.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Linq;
+
+namespace AOC19
+{
+    
+    class Aoc02 : AocBase
+    {
+        public Aoc02(string filepath) : base(filepath)
+        {
+        }
+
+        public override string ExecuteA(string[] inputs)
+        {
+            var intcodes = inputs[0].Split(',').Select(int.Parse).ToArray();
+            
+            // before run 
+            intcodes[1] = 12;
+            intcodes[2] = 2;
+            return Compute(intcodes).ToString();
+        }
+        
+        private int Compute(int[] intcodes)
+        {
+            for(int i = 0; i< intcodes.Length; i+=4)
+            {
+                var opcode = intcodes[i];
+                if(opcode == 99) 
+                {
+                    //halt
+                    break;
+                }    
+                else if(opcode == 1)
+                {
+                    //add
+                    intcodes[intcodes[i+3]] = intcodes[intcodes[i+1]] + intcodes[intcodes[i+2]];
+                }
+                else if(opcode == 2)
+                {
+                    //mult
+                    intcodes[intcodes[i+3]] = intcodes[intcodes[i+1]] * intcodes[intcodes[i+2]];
+                }
+                else
+                {
+                    Console.WriteLine("ERR!!!!!");
+                }
+
+            }
+            return intcodes[0];
+        }
+
+        public override string ExecuteB(string[] inputs)
+        {
+            var intcodes = inputs[0].Split(',').Select(int.Parse).ToArray();
+            for (int noun = 0; noun < 99; noun++)
+            {
+                for(int verb = 0; verb < 99; verb++)
+                {
+                    var local = (int[]) intcodes.Clone();
+                    local[1] = noun;
+                    local[2] = verb;
+                    if(Compute(local) == 19690720)
+                    {
+                        return (100 * noun + verb).ToString();
+                    }       
+                }
+            }
+            return "not found";
+        }
+    }
+}
---
+++ AOC19/appclass/Aoc03.cs
@@ -0,0 +1,19 @@
+namespace AOC19
+{
+    class Aoc03 : AocBase
+    {
+        public Aoc03(string filepath) : base(filepath)
+        {
+        }
+
+        public override string ExecuteA(string[] inputs)
+        {
+            return "Not Implemented";
+        }
+
+        public override string ExecuteB(string[] inputs)
+        {
+            return "Not Implemented";
+        }
+    }
+}
